d7ecf9aff655eac2ce153992e3a8df06219a1a9c
hafidFrn <hafidfaraoun27@gmail.com>
2025-09-30 23:20:41 +0100

    add test page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import "./i18n"; // Import the i18n configuration
 import LegalPages from "./pages/LegalPages";
-
+import Test from "./pages/Test";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -20,6 +20,7 @@ const App = () => (
         <Router>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/test" element={<Test />} />
             <Route path="/legal" element={<LegalPages />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
---
+++ src/pages/Test.tsx
@@ -0,0 +1,10 @@
+
+const Test = () => { 
+  return ( 
+    <div className="min-h-screen bg-background py-8"> 
+      test
+    </div> 
+  ); 
+}; 
+
+export default Test;
