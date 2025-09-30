4d6e6fd3ea4dda74ec9f34760cf7fea39f34258e
hafidFrn <hafidfaraoun27@gmail.com>
2025-09-30 23:28:05 +0100

    Remove name of Browser router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -17,14 +17,14 @@ const App = () => (
       <Toaster />
       <Sonner />
       <HelmetProvider>
-        <Router>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/test" element={<Test />} />
             <Route path="/legal" element={<LegalPages />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </Router>
+        </BrowserRouter>
       </HelmetProvider>
     </TooltipProvider>
   </QueryClientProvider>
